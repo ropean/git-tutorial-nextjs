5115f69162c492f9ed338cf6bdca87ee63c66880
ropean <i9196880@gmail.com>
2026-02-15 10:23:15 -0500

    try to deploy to worker

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,7 @@
-import { defineCloudflareConfig } from '@opennextjs/cloudflare'
+// default open-next.config.ts file created by @opennextjs/cloudflare
+import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
+import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
 export default defineCloudflareConfig({
-  // Default configuration - OpenNext will auto-configure
-})
+	incrementalCache: r2IncrementalCache,
+});
